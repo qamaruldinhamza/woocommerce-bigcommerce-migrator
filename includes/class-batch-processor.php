838667a8eca54269e2c9e1b546857169250fa346
qamaruldinhamza <qamaruldinhamza@gmail.com>
2025-09-17 05:14:37 +0500

    better prepare products system 1.1.5

--- includes/class-batch-processor.php
+++ includes/class-batch-processor.php
@@ -15,7 +15,7 @@ class WC_BC_Batch_Processor {
 
 		try {
 			$batch_size = 25; // Smaller batch size
-			$offset = 9000;
+			$offset = 9700;
 			$inserted = 0;
 			$skipped = 0;
 			$total_processed = 0;
